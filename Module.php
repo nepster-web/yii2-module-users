7412841894af759686df960805b6d8bd135f4114
Nepster <nepsterxxx@mail.ru>
2015-06-21 15:01:54 +0300

    update

--- Module.php
+++ Module.php
@@ -10,90 +10,101 @@ use Yii;
 class Module extends \yii\base\Module
 {
     /**
+     * Группа пользователя по умолчанию, которая будет присвоена при регистрации
+     */
+    public $defaultGroup = 'user';
+
+    /**
+     * Группы по умолчанию (нельзя удалять и редактировать)
+     */
+    public $defaultGroups = ['admin', 'user'];
+
+    /**
+     * Доступ ролей в панель управления
+     */
+    public $accessGroupsToControlpanel = ['admin'];
+
+    /**
+     * Кол-во попыток неправильной авторизации
+     */
+    public $amountAttemptsAuth = 10;
+
+    /**
+     * Интервал времени на которое будет заблокирован пользователь
+     */
+    public $allowedTimeAttemptsAuth = 600; // 10 минут
+
+    /**
+     * Интервал времени на который блокировать пользователя по умолчанию
+     */
+    public $intervalDefaultTimeBan = 31536000; // 365 дней
+
+    /**
+     * Интервал времени на который блокировать пользователя
+     */
+    public $intervalAuthBan = 900; // 15 минут
+
+    /**
+     * Интервал времени бездействия, после которого считается, что пользователь покинул сайт
+     */
+    public $intervalInactivityForOnline = 600; // 10 минут
+
+    /**
+     * Обязательное подтверждение почтового адреса после регистрации
+     */
+    public $requireEmailConfirmation = true;
+
+    /**
+     * Обязательное подтверждение телефона после регистрации
+     */
+    public $requirePhoneConfirmation = false;
+
+    /**
+     * Время, по истечению которого секретный ключ активации становится недействительным
+     */
+    public $activationWithin = 86400; // 24 hours
+
+    /**
+     * Время, по истечению которого секретный ключ восстановления пароля становится недействительным
+     */
+    public $recoveryWithin = 14400; // 4 hours
+
+    /**
+     * Кол-во пользователей, которое необходимо выводить на страницу
+     */
+    public $recordsPerPage = 10;
+
+    /**
+     * Путь к временной директории с аватарками пользователей
+     */
+    public $avatarsTempPath = '@statics/temp/users/avatars';
+
+    /**
+     * Путь к директории с аватарками пользователей
+     */
+    public $avatarPath = '@statics/web/users/avatars';
+
+    /**
+     * Адрес директории на сайте с аватарками пользователей
+     */
+    public $avatarUrl = '/statics/users/avatars';
+
+    /**
      * @inheritdoc
      */
+    // TODO: Переопределение текущей конфигурации конфигурацией из базы данных
+    /**
+     Переопределние значений конфигурации
     public function init()
     {
         parent::init();
 
-        /**
-         * Группа пользователя по умолчанию
-         */
-        $this->params['defaultGroup'] = 'user';
+        $reflect = new \ReflectionClass(get_called_class());
+        $props   = $reflect->getProperties(\ReflectionProperty::IS_PUBLIC);
 
-        /**
-         * Группы по умолчанию (нельзя удалять и редактировать)
-         */
-        $this->params['defaultGroups'] = ['admin', 'user'];
-
-        /**
-         * Доступ ролей в панель управления
-         */
-        $this->params['accessGroupsToControlpanel'] = ['admin', 'tester'];
-
-        /**
-         * Кол-во попыток неправильной авторизации
-         */
-        $this->params['amountAttemptsAuth'] = 10;
-
-        /**
-         * Интервал времени на которое будет заблокирован пользователь
-         */
-        $this->params['allowedTimeAttemptsAuth'] = 600; // 10 минут
-
-        /**
-         * Интервал времени на который блокировать пользователя по умолчанию
-         */
-        $this->params['intervalDefaultTimeBan'] = 31536000; // 365 дней
-
-        /**
-         * Интервал времени на который блокировать пользователя
-         */
-        $this->params['intervalAuthBan'] = 900; // 15 минут
-
-        /**
-         * Интервал времени бездействия, после которого считается, что пользователь покинул сайт
-         */
-        $this->params['intervalInactivityForOnline'] = 600; // 10 минут
-
-        /**
-         * Обязательное подтверждение почтового адреса после регистрации
-         */
-        $this->params['requireEmailConfirmation'] = true;
-
-        /**
-         * Обязательное подтверждение телефона после регистрации
-         */
-        $this->params['requirePhoneConfirmation'] = false;
-
-        /**
-         * Время, по истечению которого секретный ключ активации становится недействительным
-         */
-        $this->params['activationWithin'] = 86400; // 24 hours
-
-        /**
-         * Время, по истечению которого секретный ключ восстановления пароля становится недействительным
-         */
-        $this->params['recoveryWithin'] = 14400; // 4 hours
-
-        /**
-         * Кол-во пользователей, котое необходимо выводить на страницу
-         */
-        $this->params['recordsPerPage'] = 10;
-
-        /**
-         * Путь к временной папке с аватарками пользователей
-         */
-        $this->params['avatarsTempPath'] = '@statics/temp/users/avatars';
-
-        /**
-         * Путь к папке с аватарками пользователей
-         */
-        $this->params['avatarPath'] = '@statics/web/users/avatars';
-
-        /**
-         * Адрес папки на сайте с аватарками пользователей
-         */
-        $this->params['avatarUrl'] = '/statics/users/avatars';
-    }
+        foreach ($props as $prop) {
+            $property = $prop->getName();
+            $this->$property = 'Значение';
+        }
+    }*/
 }
--- docs/social-auth.md
+++ docs/social-auth.md
@@ -1,3 +1,35 @@
 # Авторизация  через социальные сети
 
 > **NOTE:** В разработке
+
+
+
+
+### VKontakte
+
+- Зарегистрировать новое приложение и получить доступ [тут](http://vk.com/editapp?act=create).
+
+
+### Yandex
+
+- Зарегистрировать новое приложение и получить доступ [тут](https://oauth.yandex.com/client/new).
+
+
+### Twitter
+
+- Зарегистрировать новое приложение и получить доступ [тут](https://dev.twitter.com/apps/new).
+
+
+### Facebook
+
+- Зарегистрировать новое приложение и получить доступ [тут](https://developers.facebook.com/apps).
+
+
+### Google
+
+- Зарегистрировать новое приложение и получить доступ [тут](https://console.developers.google.com/project).
+
+
+### Github
+
+- Зарегистрировать новое приложение и получить доступ [тут](https://github.com/settings/applications/new).
