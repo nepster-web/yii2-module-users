b3e94f70f669ccc2fbdea803964c9034f8dc1700
Nepster <nepsterxxx@mail.ru>
2015-06-29 16:42:53 +0300

    fix send email

--- commands/ControlController.php
+++ commands/ControlController.php
@@ -8,7 +8,7 @@ use yii\log\Logger;
 use Yii;
 
 /**
- *
+ * Class ControlController
  */
 class ControlController extends \yii\console\Controller
 {
@@ -23,55 +23,49 @@ class ControlController extends \yii\console\Controller
     private $_user;
 
     /**
-     * Send
-     * @param $userId
-     * @param $view - template view
+     * Send Email
+     *
+     * @param $email
+     * @param $view
      * @param $subject
      *
-     * Command: php Yii users/send [userId] [view] [subject]
+     * Command: php yii users/control/send-mail [email] [view] [subject]
      */
-    public function actionIndex($userId, $view, $subject)
+    public function actionSendMail($email, $view, $subject)
     {
         $this->_user = User::find()
-            ->where('id = :user_id', [':user_id' => $userId])
+            ->where('email = :email', [':email' => $email])
             ->with('profile')
             ->asArray()
             ->one();
 
         if ($this->_user) {
-            $this->sendMail($view, $subject);
+
+            $mail = Yii::$app->getMailer();
+            $mail->viewPath = $this->mailViewPath;
+            $send = $mail->compose($view, ['user' => $this->_user])
+                ->setFrom(Yii::$app->getMailer()->messageConfig['from'])
+                ->setTo($this->_user['email'])
+                ->setSubject($subject)
+                ->send();
+
+            if ($send) {
+                $this->stdout("SUCESS" . PHP_EOL, Console::FG_GREEN);
+                Yii::getLogger()->log('SUCCESS: E-MAIL: ' . $this->_user['email'] . ', view: ' . $view, Logger::LEVEL_INFO, 'users.send');
+            } else {
+                $this->stdout("ERROR" . PHP_EOL, Console::FG_RED);
+                Yii::getLogger()->log('ERROR: E-MAIL: ' . $this->_user['email'] . ', view: ' . $view, Logger::LEVEL_ERROR, 'users.send');
+            }
+
         } else {
-            Yii::getLogger()->log('ERROR: send fail. DATA: ' . $userId . ', ' . $view . ', ' . $subject, Logger::LEVEL_ERROR, 'users.send');
+            Yii::getLogger()->log('ERROR: send fail. DATA: ' . $email . ', ' . $view . ', ' . $subject, Logger::LEVEL_ERROR, 'users.send');
         }
     }
-
-
-    /**
-     * Send mail
-     */
-    private function sendMail($view, $subject)
-    {
-        $mail = Yii::$app->getMailer();
-        $mail->viewPath = $this->mailViewPath;
-        $send = $mail->compose($view, ['user' => $this->_user])
-            ->setFrom(Yii::$app->getMailer()->messageConfig['from'])
-            ->setTo($this->_user['email'])
-            ->setSubject($subject)
-            ->send();
-
-        if ($send) {
-            $this->stdout("SUCESS" . PHP_EOL, Console::FG_GREEN);
-            Yii::getLogger()->log('SUCCESS: E-MAIL: ' . $this->_user['email'] . ', view: ' . $view, Logger::LEVEL_INFO, 'users.send');
-            return;
-        }
-
-        $this->stdout("ERROR" . PHP_EOL, Console::FG_RED);
-        Yii::getLogger()->log('ERROR: E-MAIL: ' . $this->_user['email'] . ', view: ' . $view, Logger::LEVEL_ERROR, 'users.send');
-    }
-
 
     /**
      * Import access rules
+     *
+     * Command: php yii users/control/rbac
      */
     public function actionRbac()
     {
@@ -158,8 +152,10 @@ class ControlController extends \yii\console\Controller
             $this->stdout('Import access rules success' . PHP_EOL, Console::FG_GREEN, Console::UNDERLINE);
 
         } catch (\Exception $e) {
+
             $transaction->rollBack();
             $this->stdout('Import access rules error' . PHP_EOL, Console::FG_RED, Console::UNDERLINE);
+
         }
     }
 }
